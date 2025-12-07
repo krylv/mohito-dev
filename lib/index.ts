2b1e68acc442c0bcb067213dc2176923c668ab31
krylov42 <worldbalance1337@gmail.com>
2025-12-08 01:53:26 +0500

    first commit

---
+++ lib/index.ts
@@ -0,0 +1,1 @@
+export {ScrollTrigger,SplitText,gsap} from './gsap'
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {gsap} from 'lib'
+import 'src/styles/global.css'
+
+function App() {
+
+  return (
+    <div className='flex-center h-screen'>Hello,GSAP</div>
+  )
+}
+
+export default App
