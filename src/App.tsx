08aaea90d3a705f08360bd30e07046166bfecf0d
krylov42 <worldbalance1337@gmail.com>
2025-12-21 14:21:30 +0500

    dev-heroSection feat: add Footer block

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "src/styles/global.css";
 import About from "./components/About";
 import Art from "./components/Art";
 import Cocktails from "./components/Cocktails";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
@@ -17,6 +18,7 @@ function App() {
 				<About />
 				<Art />
 				<Menu />
+				<Footer />
 			</main>
 		</SmoothScroll>
 	);
