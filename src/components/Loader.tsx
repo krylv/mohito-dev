6b161099fc26150f0ff946cfc7ebf27fb4473c55
krylov42 <worldbalance1337@gmail.com>
2025-12-23 08:54:01 +0500

    dev-preloader-21.12.2025 feat: add loader, fix styles

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,31 +1,102 @@
 import { useGSAP } from "@gsap/react";
-import { useEffect, useState } from "react";
+import { gsap, SplitText } from "lib/gsap";
+import { useEffect, useRef, useState } from "react";
 
 const Loader = () => {
+	const [isContentLoaded, setIsContentLoaded] = useState(false);
 	const [isLoading, setIsLoading] = useState(true);
 	const [canEndAnimate, setCanEndAnimate] = useState(false);
 
-	useGSAP(() => {});
+	useGSAP(() => {
+		const title = SplitText.create("#logo_title", { type: "chars" });
+
+		const circleTimeline = gsap.timeline({
+			repeat: 0,
+			onComplete: () => {
+				setCanEndAnimate(true);
+			},
+		});
+
+		circleTimeline
+			.fromTo(
+				title.chars,
+				{
+					opacity: 0,
+					top: (idx) => (idx % 2 === 0 ? 100 : -100),
+					rotate: (idx) => (idx % 2 === 0 ? 180 : 180),
+				},
+				{
+					opacity: 1,
+					top: 0,
+					left: 0,
+					rotate: 0,
+					stagger: 0.3,
+					ease: "back.inOut",
+				},
+			)
+			.to(
+				".leaf",
+				{
+					opacity: 1,
+					left: (idx: number) => {
+						return idx % 2 === 0 ? 40 : 300;
+					},
+					top: (idx: number) => {
+						return idx % 2 === 0 ? 40 : 70;
+					},
+					rotate: -90,
+					stagger: 0.5,
+					ease: "power3.inOut",
+				},
+				"-=0.5",
+			);
+	});
 
 	useEffect(() => {
-		const loadContent = () => {
-			setIsLoading(false);
-			document.documentElement.classList.remove("lock_scroll");
+		document.documentElement.classList.add("lock_scroll");
+		const handleLoad = () => {
+			setIsContentLoaded(true);
 		};
+
 		if (document.readyState === "complete") {
-			setIsLoading(false);
-			return;
+			setIsContentLoaded(true);
+		} else {
+			window.addEventListener("load", handleLoad);
 		}
-		window.addEventListener("load", loadContent);
-		addEventListener("DOMContentLoaded", loadContent);
 
 		return () => {
-			window.removeEventListener("load", loadContent);
-			document.removeEventListener("DOMContentLoaded", loadContent);
+			window.removeEventListener("load", handleLoad);
+			document.documentElement.classList.remove("lock_scroll");
 		};
 	}, []);
 
-	if (isLoading) return <div className="loader">Mojito</div>;
+	useEffect(() => {
+		if (isContentLoaded && canEndAnimate) {
+			setIsLoading(false);
+			document.documentElement.classList.remove("lock_scroll");
+		}
+	}, [isContentLoaded, canEndAnimate]);
+
+	if (isLoading)
+		return (
+			<div className="loader">
+				<div className="flex box gap-[3px] justify-center items-center relative h-full w-full  p-2 rounded-2xl">
+					<h2 className="text-[120px] flex absolute uppercase" id="logo_title">
+						Mojito
+						<img
+							src="/images/leafMojito.svg"
+							alt="min leafes"
+							className="absolute size-15 top-1/2 left-1/2 opacity-0 -z-10 leaf "
+						/>
+						<img
+							src="/images/leafMojito.svg"
+							alt="min leafes"
+							className="absolute size-15 top-1/2 left-1/2  -z-10  opacity-0 leaf "
+						/>
+					</h2>
+				</div>
+			</div>
+		);
 	return;
 };
 
